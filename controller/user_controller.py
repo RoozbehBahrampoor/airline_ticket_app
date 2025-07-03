b9889e806e1792a78f1ccc1ac8f82f2fcb6366ee
roozbehbahrampoor18@gmail.com <roozbehbahrampoor18@gmail.com>
2025-07-03 19:31:00 +0330

    upload

---
+++ controller/user_controller.py
@@ -0,0 +1,38 @@
+from view.user_view import  User
+
+user_list = []
+
+class UserController:
+    def save(self,id,name,family,birth_date,user_name,password,is_locked,role ):
+        try:
+            user = User(id,name,family,birth_date,user_name,password,is_locked,role)
+            user_list.append(user)
+            return True, f"User Saved Successfully {user}"
+        except Exception as e:
+            return False, f"User Saved Failed\n{e}"
+
+
+    def edit(self,id,name,family,birth_date,user_name,password,is_locked,role ):
+        try:
+            user1 = User(id,name,family,birth_date,user_name,password,is_locked,role)
+        #     edit to database
+            return True, f"User Edited Successfully {user1}"
+        except Exception as e:
+            return False, f"User Edited Failed\n{e}"
+
+    def delete(self, id):
+        try:
+        #     remove from database
+            return True, f"Uerson Removed Successfully - {id}"
+        except Exception as e:
+            return False, f"User Removed Failed\n{e}"
+
+
+    def find_all(self):
+        try:
+            return True, user_list
+        except Exception as e:
+            return False, f"Can't Load  Users"
+
+
+    # def find_by_name_and_family(self,name, family):
---
+++ view/user_view.py
@@ -0,0 +1,45 @@
+from controller.user_controller import UserController
+from view import *
+
+class UserView:
+    def save_click(self):
+        user_controller = UserController()
+        status, message = user_controller.save(
+            self.id.get(),
+            self.name.get(),
+            self.family.get(),
+            self.birth_date.get()
+            self.user_name.get()
+            self.password.get()
+            self.is_locked.get()
+            self.role.get()
+        )
+        if status:
+            msg.showinfo("Save", message)
+        else:
+            msg.showerror("Save Error", message)
+
+    def __init__(self):
+        win = Tk()
+        win.geometry("300x300")
+
+        self.id = IntVar()
+        self.name = StringVar()
+        self.family = StringVar()
+        self.birth_day = IntVar()
+        self.user_name = StringVar()
+        self.password = StringVar()
+        self.role = StringVar()
+
+        Entry(win, textvariable=self.id).pack()
+        Entry(win, textvariable=self.name).pack()
+        Entry(win, textvariable=self.family).pack()
+        Entry(win, textvariable=self.birth_day).pack()
+        Entry(win, textvariable=self.user_name).pack()
+        Entry(win, textvariable=self.password).pack()
+        Entry(win, textvariable=self.role).pack()
+
+        Button(win, text="Save", command=self.save_click).pack()
+
+        win.mainloop()
+
