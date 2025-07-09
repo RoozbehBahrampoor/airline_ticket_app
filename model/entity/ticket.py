08effa144254772008b462fcd2b71a21b81399cc
project of class <kasraabdi2024@gmail.com>
2025-07-09 22:35:46 +0330

    controller

--- model/entity/ticket.py
+++ model/entity/ticket.py
@@ -13,17 +13,28 @@ class ticket:
         self.sold = sold
 
     def ticket(self):
-        return f"{self.id} {self.ticket_code}"
+        return (f"{self.id} {self.ticket_code} {self.origin} {self.destination} {self.airline}"
+                f" {self.start_date_time} {self.end_date_time} {self.seat_no} {self.sold}")
 
     def __repr__(self):
-        return f"{self.ticket()} {self.origin}"
+        return (f"{self.ticket()} {self.id} id: {self.ticket_code} ticket_code:  {self.origin} origine:"
+                f" {self.destination} destination: {self.airline} airline: {self.start_date_time} start_date_time:"
+                f" {self.end_date_time} end_date_time: ")
 
     def to_tuple(self):
-        return (self.id, self.ticket_code, self.origin)
+        return (self.id, self.ticket_code, self.origin, self.destination, self.airline, self.start_date_time,
+                self.end_date_time, self.seat_no, self.sold)
 
 
     def get_id(self):
         return self.id
+
+    def set_id(self,value):
+        id_validator(value)
+        self.id = value
+
+    def get_ticket_code(self):
+        return self.ticket_code
 
     def set_ticket_code(self, value):
         ticket_code_validator(value)
@@ -33,7 +44,14 @@ class ticket:
     def get_origin(self):
         return self.origin
 
-    def set_family(self, value):
+    def set_origin(self,value):
+        origin_validator(value)
+        self.origin = value
+
+    def get_destination(self):
+        return self.destination
+
+    def set_destination(self, value):
         destination_validator(value)
         self.destination = value
 
@@ -45,11 +63,47 @@ class ticket:
         self.airline = value
 
     def get_start_date_time(self):
-        get_start_date_time_validator(value)
+        return self.start_date_time
+
+    def set_start_date_time(self, value):
+        start_date_time_validator(value)
         self.start_date_time = value
 
+    def get_end_date_time(self):
+        return self.end_date_time
 
-    name = property(get_name, set_name)
-    family = property(get_family, set_family)
-    age = property(get_age, set_age)
+    def set_end_date_time(self, value):
+        end_date_time_validator(value)
+        self.end_date_time = value
+
+    def get_seat_no(self):
+        return self.seat_no
+
+    def set_seat_no(self, value):
+        seat_no_validator(value)
+        self.seat_no = value
+
+    def get_sold(self):
+        return self.sold
+
+    def set_sold(self, value):
+        sold_validator(value)
+        self.sold = value
+
+
+
+
+
+
+    id = property(get_id, set_id)
+    ticket_code = property(get_ticket_code, set_ticket_code)
+    origin = property(get_origin, set_origin)
+    destination = property(get_destination, set_destination)
+    airline = property(get_airline, set_airline)
+    start_date_time = property(get_start_date_time, set_start_date_time)
+    end_date_time = property(get_end_date_time, set_end_date_time)
+    seat_no = property(get_seat_no, set_seat_no)
+    sold = property(get_sold, set_sold)
+
+
 
--- test/ticket_test.py
+++ test/ticket_test.py
@@ -2,4 +2,5 @@ import unittest
 from model.entity.ticket import ticket
 class TestTicket(unittest.TestCase):
     def test_create(self):
-        t = ticket("John" ,"5841" ,"Usa" , )
+        t = ticket("John" ,"5841" ,"Usa" ,"London" ,"Delta Air Line" ,
+        "1/11/2025 , 23:55" ,"2/11/2025 , 05:55" ,"" ,"YES")
